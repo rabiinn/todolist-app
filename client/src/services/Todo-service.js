b2143f6fe5639883c0f493003b82e6a15546d5b6
rabiinn <rabin.adhikari@mni.thm.de>
2025-06-10 21:59:08 +0200

    setup todoservice to fetch and manage resources from server api

---
+++ client/src/services/Todo-service.js
@@ -0,0 +1,46 @@
+const baseUrl = 'http://localhost:8080/api/todos';
+
+const getAll = async () => {
+    const response = await fetch(baseUrl);
+    if(!response.ok) throw new Error("Unable to fetch the todos");
+    const todos = await response.json();
+    return todos;
+}
+
+const create =  async (newObject) => {
+    const response = await fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newObject)
+    });
+    if(!response.ok) throw new Error("Not able to create the todo");
+
+    return  await response.json();
+}
+
+const update = async (id, newObject) => {
+    const response = await fetch(`${baseUrl}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(newObject)
+    });
+    if(!response.ok) throw new Error("Not able to update the todo");
+    return await response.json();
+}
+
+const deleteTodo = async (id) => {
+    const response = await fetch(`${baseUrl}/${id}`, {
+        method: 'DELETE',
+    
+    })
+
+    if(!response.ok) throw new Error("Not able to delete the todo");
+}
+
+
+export default {getAll, create, update, deleteTodo};
+
